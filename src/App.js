664179bb901c42953ed7da4547cdc36975a732da
Keith Capers <K.Capers94@gmail.com>
2024-08-15 19:53:35 -0400

    Added some pics and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { pokemon } from './data';
 
 function App() {
+  console.log(pokemon)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> The PokePagent!!</h1>
+      <img src={pokemon} alt="Mew" />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,3 @@
+export const pokemon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhRuTwzRiVstXO1nH2OaEibTK4Wh_zWjXE_g&s"
+
+export const aboutPage = "This is the Pokemon fan page. This is a place where pokemon lover can share cool,cute,or tough pictures of all your favorite pokemon!All of us pokemon lovers always have our favorite pokemon so feel free to share your with us here on the Poke fan app!"
