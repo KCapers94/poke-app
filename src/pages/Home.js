dcafc205f7cc305413bcebd3b38ebeacda428b7e
Keith Capers <K.Capers94@gmail.com>
2024-09-02 21:59:56 -0400

    added Pic for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,14 @@ import React from "react"
 
 function Home() {
     
+    const mew = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhRuTwzRiVstXO1nH2OaEibTK4Wh_zWjXE_g&s"
+
 
     return (
        <div> 
         <main>
-             <h1>Home Page!</h1>
+             <h1>Poke Fans!</h1>
+             <img src={mew} alt="mew"/>
         </main>
        </div> 
     )
